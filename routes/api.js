338bb6942e83cb32226deaec3a9f8833b1317763
bruno918 <bruno-leal@educar.rs.gov.br>
2022-07-11 20:51:47 -0300

    nseiii

--- routes/api.js
+++ routes/api.js
@@ -298,7 +298,19 @@ router.get('/game/adivipersonagem', async (req, res, next) => {
         res.json(loghandler.invalidKey)
     }
 })
-
+router.get('/anime/amv', async (req, res, next) => {
+var Apikey = req.query.apikey
+if(!Apikey) return res.json(loghandler.notparam)
+if(listkey.includes(Apikey)){
+const amv = JSON.parse(fs.readFileSync(__path +'/data/amv.json'));
+const randAmv = amv[Math.floor(Math.random() * amv.length)];
+data = await fetch(randAmv).then(v => v.buffer())
+await fs.writeFileSync(__path +'/tmp/amv.mp4', data)
+res.sendFile(__path +'/tmp/amv.mp4')
+} else {
+res.json(loghandler.invalidKey)
+}
+})
 
 router.get('/download/ytmp3', async(req, res, next) => {
   const url = req.query.url;
