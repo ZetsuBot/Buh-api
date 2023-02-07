09ba9b74b1832b91b5219a4e0fa4d338005fd5ea
Clovermods <111981866+CloverMods@users.noreply.github.com>
2023-02-07 16:04:36 -0300

    Add files via upload

---
+++ database/index.js
@@ -0,0 +1,8 @@
+const { musicallydown, ssstik, keeptiktok, tiktokdownload, tiklydown, dlpanda } = require('./tt.js')
+
+exports.musicallydown = musicallydown
+exports.ssstik = ssstik
+exports.keeptiktok = keeptiktok
+exports.tiktokdownload = tiktokdownload
+exports.tiklydown = tiklydown
+exports.dlpanda = dlpanda
