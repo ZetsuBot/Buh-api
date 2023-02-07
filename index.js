e655b946be84349fcad9756b690a6dfc4445315c
Clovermods <111981866+CloverMods@users.noreply.github.com>
2023-02-07 16:06:25 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 //yy
-__path = process.cwd();
+//__path = process.cwd();
 //var favicon = require('serve-favicon');
 var express = require('express'),
     cors = require('cors'),
     secure = require('ssl-express-www');
-const PORT = process.env.PORT || 8080 || 5000 || 3000
-var { color } = require('./lib/color.js')
-
+//const PORT = p 8080 || 5000 || 3000
+//var { color } = require('./lib/color.js')
+const logger = require('morgan');
 //////////////////////////////
 var mainrouter = require('./routes/main'),
     apirouter = require('./routes/api')
@@ -16,14 +16,15 @@ var app = express()
 app.enable('trust proxy');
 app.set("json spaces",2)
 app.use(cors())
+app.use(logger('dev'));
 app.use(secure)
 app.use(express.static("public"))
 
 app.use('/', mainrouter);
 app.use('/api', apirouter);
 
-app.listen(PORT, () => {
-    console.log(color("Server rodando na porta:" + PORT,'green'))
+app.listen(8000, () => {
+    console.log("Server rodando na porta 8000")
 })
 
 module.exports = app
